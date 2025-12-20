7a857251ad5642553b890d445948b1adfbe34122
1m-C00l <nawmayjue2013@gmail.com>
2025-12-20 22:11:03 +0630

    setup project

---
+++ src/main/java/com/springboot/jobseeker/welcome/controller/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.springboot.jobseeker.welcome.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1/welcome")
+@RestController
+public class WelcomeController {
+    @GetMapping
+    public String welcomeMessage(){
+        return "Hello";
+    }
+}
